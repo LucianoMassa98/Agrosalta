99b427000daf6e9317a33841c9463d04f356d137
lucianomassa98 <lgmassa98@gmail.com>
2023-06-06 01:37:17 -0300

    modificar login

--- src/schemas/login.schema.js
+++ src/schemas/login.schema.js
@@ -2,11 +2,12 @@ const joi = require('joi');
 
 const username = joi.string().min(3);
 const password = joi.string().min(3);
-
+const oficinaId = joi.number().integer();
 
 const signInSchema = joi.object({
   username: username.required(),
-  password: password.required()
+  password: password.required(),
+  oficinaId: oficinaId.required()
 });
 
 module.exports = {
--- src/services/login.service.js
+++ src/services/login.service.js
@@ -6,14 +6,25 @@ class loginService{
     async signIn(body){
 
       const options= { where:{}}
-      const {username, password}=body;
+      const {username, password, oficinaId}=body;
       options.where.username= username;
       options.where.password= password;
       
       const res  = await models.Usuario.findOne(options);
       if(!res){ throw boom.notFound('Usuario Not Found');}
+      const res2 = await models.Oficina.findByPk(oficinaId,{
+        include:['movimientos'],
+      });
+      var estado = true;
 
-      return res;
+      if(!res2){throw boom.notFound('Movimientos not found');}
+      if(res2.movimientos[0].descripcion=="Cierre de caja"){estado=false;}
+
+
+      return {
+              ...res.dataValues, 
+              estado: estado
+              };
     }
     
   
